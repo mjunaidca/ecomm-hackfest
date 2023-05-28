928fa9c2a60834b2ad49de58f756c00081517c29
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-05-28 16:07:24 +0500

    feat desktop navbar

--- src/components/Header/MainNav/NavBar.tsx
+++ src/components/Header/MainNav/NavBar.tsx
@@ -3,7 +3,7 @@ import LOGO from "@/assets/LOGO.png";
 import Image from "next/image";
 import Link from "next/link";
 import { BsCart } from "react-icons/bs";
-import {CiSearch} from "react-icons/ci"
+import { CiSearch } from "react-icons/ci";
 
 export const navigation = [
   {
@@ -26,51 +26,26 @@ export const navigation = [
 
 export const NavBar = () => {
   return (
-    <header className="container flex items-center py-3 px-16 mx-auto justify-between">
+    <header className="container flex items-center py-6 md:py-3 px-4 sm:px-8 md:px-12 lg:px-16 mx-auto justify-between">
       <div>
         {/* Logo */}
-        <Image
-          src={LOGO}
-          alt="Dine Marketplace"
-          className="w-20 h-7 md:h-6 md:w-36"
-        />
+        <Image src={LOGO} alt="Dine Marketplace" className="h-6 w-36" />
       </div>
-      <div>
-        {/* Main Navigation */}
-        <div className={`p-6 flex flex-col gap-y-3 text-base font-medium  `}>
-          <ul className="items-center flex justify-center md:space-x-8 lg:space-x-9 xl:space-x-12">
-            {navigation.map((item: any, idx: any) => {
-              return (
-                <li key={idx} className="text-gray-800 hover:text-gray-600">
-                  <Link
-                    href={item.path}
-                    className="flex items-center gap-x-4 z-10"
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+        {/* Main Desktop Navigation */}
+        <div
+          className={` hidden md:flex p-6  flex-col gap-y-3 text-base font-medium  `}
+        >
+          <DesktopNavigation />
         </div>
+      {/* Search Bar */}
+      <div className="  w-full hidden lg:block lg:max-w-[300px] xl:max-w-sm">
+        <SearchBar />
       </div>
-      <div className='  w-full lg:max-w-xs xl:max-w-sm'>
-        {/* Search Bar */}
-
-        <SearchBar  />
-      
-      </div>
-      <div>{/* User Info */}</div>
-      <div>
         {/* Cart */}
-        <Link href="">
-          <div className="rounded-full">
-            <div className="rounded-full bg-gray-200 p-3 hover:bg-gray-200 ">
-              <BsCart className="text-black w-5 h-5 rounded-full  hover:scale-110" />
-            </div>
-          </div>
-        </Link>
+      <div  className="hidden md:flex">
+          <Cart />
       </div>
+        <div>{/* Mobile View */}</div>
     </header>
   );
 };
@@ -89,5 +64,36 @@ const SearchBar = () => {
         placeholder="What are you looking for?"
       />
     </div>
+  );
+};
+
+const DesktopNavigation = () => {
+  return (
+    <ul className="items-center flex justify-center md:space-x-8 lg:space-x-9 xl:space-x-12">
+      {navigation.map((item: any, idx: any) => {
+        return (
+          <li key={idx} className="text-gray-800 hover:text-gray-600">
+            <Link
+              href={item.path}
+              className="flex items-center lg:gap-3 xl:gap-x-4 z-10"
+            >
+              {item.title}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+const Cart = () => {
+  return (
+    <Link href="">
+      <div className="rounded-full">
+        <div className="rounded-full bg-gray-200 p-3 hover:bg-gray-200 ">
+          <BsCart className="text-black w-5 h-5 rounded-full  hover:scale-110" />
+        </div>
+      </div>
+    </Link>
   );
 };
