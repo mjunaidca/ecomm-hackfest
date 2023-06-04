e309f32362e85cac75e5569b2de6f7743877cd97
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-06-04 13:35:47 +0500

    cart checklist and res vercel error

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { client } from "@/lib/sanityClient";
 import CartItemsCard from "@/components/shared/CartItemsCard";
+import { Button } from "@/components/ui/button";
 
 interface Product {
   id: number;
@@ -11,7 +12,7 @@ interface Product {
 
 type ProductList = Product[];
 
-export async function getCartData() {
+async function getCartData() {
   const res = await fetch(
     `${process.env.Base_Url}/api/cart?user_id=${
       cookies().get("user_id")?.value
@@ -25,7 +26,7 @@ function getProductIds(cartData: any[]): string[] {
   return cartData.map((item) => item.product_id);
 }
 
-export async function getCartProductDetails(productIds: string[]) {
+async function getCartProductDetails(productIds: string[]) {
   const products = await client.fetch(
     `*[_type == 'product' && _id in $productIds]`,
     { productIds }
@@ -34,7 +35,7 @@ export async function getCartProductDetails(productIds: string[]) {
   return products;
 }
 
-const CartPage = async () => {
+export default async function CartPage() {
   const querycartData = await getCartData();
   const cartData: ProductList = querycartData.res;
 
@@ -43,22 +44,41 @@ const CartPage = async () => {
   const productDetails: Product[] = await getCartProductDetails(productIds);
 
   return (
-    <main className="container flex items-start justify-between min-h-screen">
+    <main className="container min-h-screen pb-1 px-2 mb-8 sm:px-5 md:px-10 lg:px-12 xl:px-16 py-20">
       {/* Products In Cart */}
-      <div>
-        {productDetails.map((product: any, index: any) => (
-          <div
-            key={index}
-            className="transition-transform duration-700 hover:scale-105"
-          >
-            <CartItemsCard product={product} />
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Products In Cart
+      </h3>
+      <div className="flex py-6 flex-wrap sm:flex-nowrap items-start justify-between">
+        <div className="basis-2/3 w-full">
+          {productDetails.map((product: any, index: any) => (
+            <div
+              key={index}
+              className="transition-transform duration-700 hover:scale-105"
+            >
+              <CartItemsCard product={product} />
+            </div>
+          ))}
+        </div>
+        {/* Cart Summary */}
+        <div className="w-full p-6 bg-gray-100 sm:w-auto">
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            Order Summary
+          </h4>
+          <div className="flex justify-between  space-x-4items-center [&:not(:first-child)]:mt-6">
+            <p className="leading-7 ">Quanity</p>
+            <p className="leading-7 ">Products</p>
           </div>
-        ))}
+          <div className="flex justify-between items-center space-x-4 [&:not(:first-child)]:mt-6">
+            <p className="leading-7 ">SubTotal</p>
+            <p className="leading-7 ">$</p>
+          </div>
+          <Button className="rounded-none px-10 [&:not(:first-child)]:mt-6">
+            {" "}
+            Process to checkout{" "}
+          </Button>
+        </div>
       </div>
-      {/* Cart Summary */}
-      <div>Cart Summary</div>
     </main>
   );
-};
-
-export default CartPage;
+}
--- src/components/shared/CartItemsCard.tsx
+++ src/components/shared/CartItemsCard.tsx
@@ -1,11 +1,50 @@
 import React from "react";
 import { FC } from "react";
+import AddToCartForm from "@/components/shared/AddToCartForm";
+import { client } from "@/lib/sanityClient";
+import urlFor from "@/lib/urlFor";
+import Image from "next/image";
 
 const CartItemsCard: FC<{ product: any }> = ({ product }) => {
   return (
     <div>
-      CartItemsCard
       {/* Produt Details */}
+      {/* Image */}
+      <div className="flex items-center">
+        <div className="lg:basis-3/5 flex basis-full space-x-10">
+          <div className="space-y-5 ">
+            <div>
+              <Image
+                src={urlFor(product.mainImage).url()}
+                width={225}
+                height={350}
+                alt={product.mainImage.alt}
+                className="max-h-420px] max-w-[275px] w-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+        {/* Checkout Details */}
+        <div className="lg:basis-2/5 basis-full py-16 px-2 lg:px-5 xl:px-8 space-y-8">
+          {/* Heading */}
+          <div>
+            <h2 className="scroll-m-20 pb-1  text-2xl lg:text-3xl font-normal tracking-tight transition-colors first:mt-0">
+              {product.title}
+            </h2>
+            <h3 className="scroll-m-20 text-xl lg:text-2xl font-semibold text-gray-400 tracking-tight">
+              {product.type}
+            </h3>
+          </div>
+          {/* Size */}
+          <div>
+            <h4 className="scroll-m-20 text-lg font-semibold ">Select Size</h4>
+          </div>
+          {/* Quantity */}
+          <div>
+            <h4 className="scroll-m-20 text-lg font-semibold ">Quantity</h4>
+          </div>
+        </div>
+      </div>
       <div></div>
       {/* Delete and Order Count */}
       <div></div>
